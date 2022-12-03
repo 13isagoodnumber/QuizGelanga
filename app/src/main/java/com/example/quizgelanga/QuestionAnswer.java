6989b1c13c7177f1e65ee92a33cad648fe0cfeba
LearningTask3.2_GelangaPJS <pgelanga@gmail.com>
2022-12-03 22:50:12 +0800

    Quiz Gelanga

---
+++ app/src/main/java/com/example/quizgelanga/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizgelanga;
+
+public class QuestionAnswer {
+
+    public static String question [] ={
+            "Main villain in the Silence of the Lambs?",
+            "Taylor Swift's RED Album is dedicated to?",
+            "Which is a A24 Film?"
+
+    };
+
+    public static String choices [][]={
+            {"Joker","Frozone", "Hannibal Lecter", "Thanos"},
+            {"Jake Gyllenhaal", "Taylor Lautner", "Harry Styles", "Joe Jonas"},
+            {"Eternals", "Black Swan", "Up", "Midsommar"}
+
+    };
+
+    public static String correctAnswers [] = {
+            "Hannibal Lecter",
+            "Jake Gyllenhaal",
+            "Midsommar"
+
+    };
+}
